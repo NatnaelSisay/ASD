1dc04925b951127e2e6c68a33a40f4be1d5f8fbd
NatnaelSisay <natyjava8@gmail.com>
2024-04-01 17:21:38 -0500

    remove dead code for xml conversion

--- lab-1a/ProductManagementApp/productmgmtapp/src/main/java/edu/miu/cs/cs489appsd/lab1a/productmgmntapp/model/Products.java
+++ lab-1a/ProductManagementApp/productmgmtapp/src/main/java/edu/miu/cs/cs489appsd/lab1a/productmgmntapp/model/Products.java
@@ -3,10 +3,9 @@ package edu.miu.cs.cs489appsd.lab1a.productmgmntapp.model;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.xml.bind.annotation.XmlRootElement;
 import java.util.List;
 
-@XmlRootElement
+
 @Getter
 @Setter
 public class Products {
