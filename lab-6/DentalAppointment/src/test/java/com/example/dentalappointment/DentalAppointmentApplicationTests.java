36b9b628c001009e8409b690914164e96f934f57
NatnaelSisay <natyjava8@gmail.com>
2024-04-10 14:04:49 -0500

    lab 6 init

---
+++ lab-6/DentalAppointment/src/test/java/com/example/dentalappointment/DentalAppointmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dentalappointment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DentalAppointmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
