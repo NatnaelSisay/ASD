1b8c6a0b649b090a9a39d6290a555cbf9c37736e
NatnaelSisay <natyjava8@gmail.com>
2024-04-13 15:52:00 -0500

    added timestamp to response message

--- lab-7/RESTDentalAppointment/src/main/java/com/example/dentalappointment/exceptions/GlobalExceptionHandler.java
+++ lab-7/RESTDentalAppointment/src/main/java/com/example/dentalappointment/exceptions/GlobalExceptionHandler.java
@@ -6,13 +6,16 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ItemNotFound.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ResponseEntity<ItemNotFoundResponse> handleItemNotFoundException(ItemNotFound error) {
-        ItemNotFoundResponse result = new ItemNotFoundResponse(error.getMessage(), 404);
+        ItemNotFoundResponse result = new ItemNotFoundResponse(error.getMessage(), HttpStatus.NOT_FOUND, ZonedDateTime.now(ZoneId.of("Z")));
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(result);
     }
 
--- lab-7/RESTDentalAppointment/src/main/java/com/example/dentalappointment/exceptions/ItemNotFoundResponse.java
+++ lab-7/RESTDentalAppointment/src/main/java/com/example/dentalappointment/exceptions/ItemNotFoundResponse.java
@@ -1,7 +1,12 @@
 package com.example.dentalappointment.exceptions;
 
+import org.springframework.http.HttpStatus;
+
+import java.time.ZonedDateTime;
+
 public record ItemNotFoundResponse(
         String message,
-        Integer statusCode
+        HttpStatus statusCode,
+        ZonedDateTime timestamp
 ) {
 }
