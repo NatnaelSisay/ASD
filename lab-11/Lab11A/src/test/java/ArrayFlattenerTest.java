94179834d9843dd60094ef9c7098a9b1eb0f4d14
NatnaelSisay <natyjava8@gmail.com>
2024-04-19 08:54:08 -0500

    inital commit for lab11A

---
+++ lab-11/Lab11A/src/test/java/ArrayFlattenerTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ArrayFlattenerTest {
+    @Test
+    public void test_test_is_working() {
+        assertTrue(true);
+    }
+}
