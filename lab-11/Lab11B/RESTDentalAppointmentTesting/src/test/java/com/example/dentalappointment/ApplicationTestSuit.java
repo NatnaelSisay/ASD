b02ad82250eb8c9dc4abf79f0ce0525b8e82250f
NatnaelSisay <natyjava8@gmail.com>
2024-04-20 12:03:42 -0500

    added dependencies for junit5 and mockito

---
+++ lab-11/Lab11B/RESTDentalAppointmentTesting/src/test/java/com/example/dentalappointment/ApplicationTestSuit.java
@@ -0,0 +1,5 @@
+package com.example.dentalappointment;
+
+
+public class ApplicationTestSuit {
+}
---
+++ lab-11/Lab11B/RESTDentalAppointmentTesting/src/test/java/com/example/dentalappointment/services/TestPatientService.java
@@ -0,0 +1,11 @@
+package com.example.dentalappointment.services;
+
+import org.junit.jupiter.api.Test;
+
+
+public class TestPatientService {
+    @Test
+    public void check() {
+
+    }
+}
