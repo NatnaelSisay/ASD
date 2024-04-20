17b121b2e23b74b574a89734fff239f22c51bfaf
NatnaelSisay <natyjava8@gmail.com>
2024-04-20 12:05:57 -0500

    added test suit

--- lab-11/Lab11B/RESTDentalAppointmentTesting/src/test/java/com/example/dentalappointment/ApplicationTestSuit.java
+++ lab-11/Lab11B/RESTDentalAppointmentTesting/src/test/java/com/example/dentalappointment/ApplicationTestSuit.java
@@ -1,5 +1,14 @@
 package com.example.dentalappointment;
 
 
+import com.example.dentalappointment.services.TestPatientService;
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+
+@SelectClasses({
+        TestPatientService.class,
+        DentalAppointmentApplicationTests.class
+})
+@Suite
 public class ApplicationTestSuit {
 }
