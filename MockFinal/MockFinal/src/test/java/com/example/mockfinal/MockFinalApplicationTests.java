be4544ed18645e5b4c2cebae5691949307d7dfb9
NatnaelSisay <natyjava8@gmail.com>
2024-04-21 16:34:12 -0500

    Initalize mock final environment

---
+++ MockFinal/MockFinal/src/test/java/com/example/mockfinal/MockFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mockfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockFinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
