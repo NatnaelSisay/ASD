1880803c69470b3640a192cb9899d1a0a0de3af3
NatnaelSisay <natyjava8@gmail.com>
2024-04-23 03:51:15 -0500

    clean controller and config

--- lab-9/RESTDentalAppointmentSecurity/src/main/java/com/example/dentalappointment/security/AuthController.java
+++ lab-9/RESTDentalAppointmentSecurity/src/main/java/com/example/dentalappointment/security/AuthController.java
@@ -1,2 +1,29 @@
-package com.example.dentalappointment.security;public class AuthController {
+package com.example.dentalappointment.security;
+
+import com.example.dentalappointment.security.models.LoginRequest;
+import com.example.dentalappointment.security.models.LoginResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    LoginService loginService;
+
+    public AuthController(
+            LoginService loginService
+    ) {
+        this.loginService = loginService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(
+            @RequestBody LoginRequest loginRequest
+    ) {
+        LoginResponse response = this.loginService.login(loginRequest);
+        return ResponseEntity.ok(response);
+    }
 }
--- lab-9/RESTDentalAppointmentSecurity/src/main/java/com/example/dentalappointment/security/SecurityConfig.java
+++ lab-9/RESTDentalAppointmentSecurity/src/main/java/com/example/dentalappointment/security/SecurityConfig.java
@@ -1,2 +1,30 @@
-package com.example.dentalappointment.security;public class SecurityConfig {
+package com.example.dentalappointment.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http = http
+                .cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(
+                        auth -> {
+                            auth.requestMatchers("/auth/**").permitAll();
+//                            auth.anyRequest().permitAll();
+                            auth.anyRequest().authenticated();
+                        }
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+    }
 }
