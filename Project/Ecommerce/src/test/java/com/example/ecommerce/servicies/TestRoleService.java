8c4559edbcfd1c29cd3920a403bc8f3a2fa4b0c3
NatnaelSisay <natyjava8@gmail.com>
2024-04-25 13:49:42 -0500

    pass profile to determine running environment

--- Project/Ecommerce/src/test/java/com/example/ecommerce/servicies/TestRoleService.java
+++ Project/Ecommerce/src/test/java/com/example/ecommerce/servicies/TestRoleService.java
@@ -4,11 +4,13 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(MockitoExtension.class)
 @SpringBootTest
+@ActiveProfiles("test")
 public class TestRoleService {
 
     @Test
