dd0f8a68d5df904a7e753d75df6d11533b21346f
NatnaelSisay <natyjava8@gmail.com>
2024-04-27 05:49:30 -0500

    post and image configuration

--- Project/Ecommerce/build.sh
+++ Project/Ecommerce/build.sh
@@ -1,9 +1,13 @@
 # make sure mysql is running, look at application-*.properties for reference
+docker container kill $(docker ps -aq)
+docker run -e POSTGRES_PASSWORD=examplepassword -p 5432:5432 -d postgres
 
-mvn clean package -P test
+mvn clean package -P prod
 
 docker build -t ecommerce-server .
 
 # push the image to the registry
+docker tag ecommerce-server:latest javaethio/ecommerce-server:latest
+docker push javaethio/ecommerce-server:latest
 
 docker-compose up -d --build
